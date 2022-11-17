20d37015d209bf48d400e10265eac36e68d2d09a
Hyewon Lee <113076205+wonprogrammer@users.noreply.github.com>
2022-11-17 14:46:05 +0900

    remove secret key

--- drftimeattack/settings.py
+++ drftimeattack/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*kzzye$mz#-*8(xuuz6i*=myz4)n5!-)!+_jh*+sriu0z3i0d_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
